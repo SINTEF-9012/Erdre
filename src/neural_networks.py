f2fd46e3aa8dd6b4b5f90e2b586ff74df6e3417e
ejhusom <ejlgkvam@online.no>
2022-02-25 13:03:09 +0100

    Rolling DNN back to previous version

--- src/neural_networks.py
+++ src/neural_networks.py
@@ -179,7 +179,7 @@ def dnn(
     model = models.Sequential()
     model.add(layers.Dense(16, activation="relu", input_dim=input_x))
     # model.add(layers.Dense(256, activation='relu', input_dim=input_x))
-    # model.add(layers.Dense(16, activation="relu"))
+    model.add(layers.Dense(16, activation="relu"))
     # model.add(layers.Dense(32, activation='relu'))
     # model.add(layers.Dense(32, activation='relu'))
     model.add(layers.Dense(output_length, activation=output_activation))
